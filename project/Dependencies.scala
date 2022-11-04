ff54e5952c915a45c26c765085e38d1de21c8d74
Ali Mahmood <ali.mahmood@digital.hmrc.gov.uk>
2022-11-04 09:56:43 +0000

    Removing play 2.7
    Signed-off-by: Ali Mahmood <ali.mahmood@digital.hmrc.gov.uk>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,19 +1,11 @@
 import sbt._
 
 object Dependencies {
-
-  private val play25Version = "2.5.19" //not used
-  private val play26Version = "2.6.20" //not used
-  private val play27Version = "2.7.4"
   private val play28Version = "2.8.8"
 
   val compile: Seq[ModuleID] =
     PlayCrossCompilation.dependencies(
       shared = Seq(),
-      play27 = Seq(
-        "com.typesafe.play" %% "play-ws" % play27Version % "provided",
-        "com.typesafe.play" %% "play-json" % "2.7.4" % "provided"
-      ),
       play28 = Seq(
         "com.typesafe.play" %% "play-ws" % play28Version % "provided",
         "com.typesafe.play" %% "play-json" % "2.9.2" % "provided"
@@ -26,10 +18,6 @@ object Dependencies {
         "org.scalatest"  %% "scalatest"     % "3.2.9"  % Test,
         "org.pegdown" % "pegdown" % "1.6.0" % Test,
         "com.vladsch.flexmark" % "flexmark-all" % "0.35.10"
-      ),
-      play27 = Seq(
-        "com.typesafe.play"      %% "play-test"          % play27Version % Test,
-        "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3"       % Test
       ),
       play28 = Seq(
         "com.typesafe.play"      %% "play-test"          % play28Version % Test,
